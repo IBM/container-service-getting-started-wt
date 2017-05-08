e8fddb825cfab027cfed78351484b4821f2d53ce
Coleman Jackson <ColemanIBMDevMachine@colemans-mbp.raleigh.ibm.com>
2017-05-08 13:09:49 -0400

    changed references to stage directories for initial testing

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Create Guestbook"
+echo "Create Demo Application"
 IP_ADDR=$(bx cs workers $CLUSTER_NAME | grep normal | awk '{ print $2 }')
 if [ -z $IP_ADDR ]; then
   echo "$CLUSTER_NAME not created or workers not ready"
@@ -15,21 +15,21 @@ if [ $? -ne 0 ]; then
 fi
 eval "$exp"
 
-echo -e "Downloading guestbook yml"
-curl --silent "https://raw.githubusercontent.com/kubernetes/kubernetes/master/examples/guestbook/all-in-one/guestbook-all-in-one.yaml" > guestbook.yml
+echo -e "Downloading Stage 3 Watson Deployment yml"
+curl --silent "https://raw.githubusercontent.com/IBM/container-service-getting-started-wt/master/Stage3/watson-deployment.yml" > watson-deployment.yml
 
-#Find the line that has the comment about the load balancer and add the nodeport def after this
-let NU=$(awk '/^  # type: LoadBalancer/{ print NR; exit }' guestbook.yml)+3
-NU=$NU\i
-sed -i "$NU\ \ type: NodePort" guestbook.yml #For OSX: brew install gnu-sed; replace sed references with gsed
+# #Find the line that has the comment about the load balancer and add the nodeport def after this
+# let NU=$(awk '/^  # type: LoadBalancer/{ print NR; exit }' guestbook.yml)+3
+# NU=$NU\i
+# sed -i "$NU\ \ type: NodePort" guestbook.yml #For OSX: brew install gnu-sed; replace sed references with gsed
 
 echo -e "Deleting previous version of guestbook if it exists"
-kubectl delete --ignore-not-found=true   -f guestbook.yml
+kubectl delete --ignore-not-found=true   -f watson-deployment.yml
 
 echo -e "Creating pods"
 kubectl create -f guestbook.yml
 
-PORT=$(kubectl get services | grep frontend | sed 's/.*:\([0-9]*\).*/\1/g')
+PORT=$(kubectl get services | grep watson-service | sed 's/.*:\([0-9]*\).*/\1/g')
 
 echo ""
 echo "View the guestbook at http://$IP_ADDR:$PORT"
