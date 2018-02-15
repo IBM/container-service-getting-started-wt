ac47c8232c6a5909b098a71cd9fdd90b695c4bef
Morgan Bauer <mbauer@us.ibm.com>
2018-02-15 19:52:36 +0100

    print the hostname
    Hostname is useful to disambiguate containers. This is useful to see the
    scaling as well as rollouts on image changes.

--- Lab 1/app.js
+++ Lab 1/app.js
@@ -1,8 +1,10 @@
 var express = require('express')
+var os = require("os");
+var hostname = os.hostname();
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello world! Your app is up and running in a cluster!\n')
+  res.send('Hello world from ' + hostname + '! Your app is up and running in a cluster!\n')
 })
 app.listen(8080, function() {
   console.log('Sample app is listening on port 8080.')
